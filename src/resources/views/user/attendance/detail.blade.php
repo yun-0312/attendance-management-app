bb16aeb8a5dda0d87172b06cdd73e4ad1ebbaec3
yun-0312 <yumiko_03122000@yahoo.co.jp>
2025-11-28 06:37:02 +0900

    detail.blade修正

--- src/resources/views/user/attendance/detail.blade.php
+++ src/resources/views/user/attendance/detail.blade.php
@@ -14,50 +14,50 @@
     <form class="detail__form" action="{{ route('attendance.update', ['attendance' => $attendance->id])}}" method="post">
         @csrf
         @method('PATCH')
-<table class="detail-table">
-            <tr>
-                <th>名前</th>
-                <td>{{ auth()->user()->name }}</td>
+        <table class="detail-table">
+            <tr class="detail-table__column">
+                <th class="detail-table__header">名前</th>
+                <td class="detail-table__data">{{ auth()->user()->name }}</td>
             </tr>
-            <tr>
-                <th>日付</th>
-                <td>
-                    {{ $attendance->work_date->format('Y年') }}
-                    {{ $attendance->work_date->format('n月j日') }}
+            <tr class="detail-table__column">
+                <th class="detail-table__header">日付</th>
+                <td class="detail-table__data">
+                    <span class="detail-table__data-item">{{ $attendance->work_date->format('Y年') }}</span>
+                    <span class="detail-table__data-item">{{ $attendance->work_date->format('n月j日') }}</span>
                 </td>
             </tr>
-            <tr>
-                <th>出勤・退勤</th>
-                <td>
+            <tr class="detail-table__column">
+                <th class="detail-table__header">出勤・退勤</th>
+                <td class="detail-table__data">
                     <input type="time" name="clock_in"
                         value="{{ $attendance->clock_in->format('H:i') }}"> ～
                     <input type="time" name="clock_out"
                         value="{{ $attendance->clock_out ? $attendance->clock_out->format('H:i') : '' }}">
                 </td>
             </tr>
-            <tr>
-                <th>休憩</th>
-                <td>
-                    @foreach($breaks as $i => $break)
-                        <input type="time" name="breaks[{{ $i }}][start]" 
-                            value="{{ old("breaks.$i.start", $break->break_start->format('H:i')) }}">
-                            ～
-                        <input type="time" name="breaks[{{ $i }}][end]"
-                            value="{{ old("breaks.$i.end", optional($break->break_end)->format('H:i')) }}">
-                    @endforeach
+            @foreach($breaks as $i => $break)
+            <tr class="detail-table__column">
+                <th class="detail-table__header">休憩{{ ++$i }}</th>
+                <td class="detail-table__data">
+                    <input type="time" name="breaks[{{ $i }}][start]"
+                        value="{{ old("breaks.$i.start", $break->break_start->format('H:i')) }}">
+                    ～
+                    <input type="time" name="breaks[{{ $i }}][end]"
+                        value="{{ old("breaks.$i.end", optional($break->break_end)->format('H:i')) }}">
                 </td>
             </tr>
-            <tr>
-                <th>休憩2</th>
-                <td>
+            @endforeach
+            <tr class="detail-table__column">
+                <th class="detail-table__header">休憩{{ count($breaks) + 1 }}</th>
+                <td class="detail-table__data">
                     <input type="time" name="breaks[{{ count($breaks) }}][start]">
-                        ～
+                    ～
                     <input type="time" name="breaks[{{ count($breaks) }}][end]">
                 </td>
             </tr>
-            <tr>
-                <th>備考</th>
-                <td>
+            <tr class="detail-table__column">
+                <th class="detail-table__header">備考</th>
+                <td class="detail-table__data">
                     <textarea name="reason" rows="4" class="detail-textarea">{{ old('reason') }}</textarea>
                 </td>
             </tr>
