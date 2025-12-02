27414e8aec56c826a62c2db632c2b9007e2504a3
yun-0312 <yumiko_03122000@yahoo.co.jp>
2025-12-03 05:18:14 +0900

    勤怠詳細処理作成中

--- src/app/Http/Controllers/Admin/AttendanceController.php
+++ src/app/Http/Controllers/Admin/AttendanceController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Attendance;
 use App\Models\User;
 use Carbon\Carbon;
+use App\Http\Requests\UpdateAttendanceRequest;
 
 class AttendanceController extends Controller
 {
@@ -19,5 +20,26 @@ class AttendanceController extends Controller
             ->orderBy('user_id')
             ->get();
         return view('admin.attendance.list', compact('attendances', 'date'));
+    }
+
+    public function detail (Attendance $attendance) {
+        if (!auth('admin')->check()) {
+            abort(403);
+        }
+        $breaks = $attendance->breakTimes()->orderBy('break_start')->get();
+        return view('admin.attendance.detail', compact('attendance', 'breaks'));
+    }
+
+    public function update (UpdateAttendanceRequest $request, Attendance $attendance) {
+        if (!$attendance->isChangedFromOriginal($request)) {
+            return redirect()
+                ->route('admin.attendance.detail', $attendance->id)
+                ->with('success', '変更がありませんでした');
+        }
+        // 変更があるときだけ Request を作る
+        $attendance->createRequestFromUpdate($request);
+        return redirect()
+            ->route('attendance.detail', $attendance->id)
+            ->with('success', '勤怠を更新しました');
     }
 }
