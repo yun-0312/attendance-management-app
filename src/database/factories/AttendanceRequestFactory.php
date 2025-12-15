13d51e6c56a3668c546a4cab9b60202c47ad57b3
yun-0312 <yumiko_03122000@yahoo.co.jp>
2025-12-16 08:09:12 +0900

    不要な記述削除

--- src/database/factories/AttendanceRequestFactory.php
+++ src/database/factories/AttendanceRequestFactory.php
@@ -4,9 +4,7 @@ namespace Database\Factories;
 
 use App\Models\AttendanceRequest;
 use App\Models\Attendance;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 use Carbon\Carbon;
 
 class AttendanceRequestFactory extends Factory
